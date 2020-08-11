a8dc0686ba2ae9faa84034deee25780d150bfce4
wanglinquan <wanglinquan@yy.com>
2020-08-11 12:24:49 +0800

    build: add deps

---
+++ package-scripts.js
@@ -0,0 +1,10 @@
+// const npsUtils = require('nps-utils')
+
+module.exports = {
+  scripts: {
+    default: 'vue-cli-service serve',
+    build: 'vue-cli-service build',
+    lint: 'vue-cli-service lint',
+    svg: 'node scripts/compile-svg.js'
+  }
+}
